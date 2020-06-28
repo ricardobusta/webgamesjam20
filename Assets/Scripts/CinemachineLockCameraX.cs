bafb695fde528723d0a599665f1a21f005bc7a04
Ricardo Bustamante <ricardo@busta.dev>
2020-06-28 18:01:46 +0200

    Meh

---
+++ Assets/Scripts/CinemachineLockCameraX.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace BustaGames.Climber
+{
+    [ExecuteInEditMode]
+    [SaveDuringPlay]
+    [AddComponentMenu("")] // Hide in menu
+    public class CinemachineLockCameraX : CinemachineExtension
+    {
+        [Tooltip("Lock the camera's X position to this value")]
+        public float xPosition;
+
+        protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            if (enabled && stage == CinemachineCore.Stage.Body)
+            {
+                var pos = state.RawPosition;
+                pos.x = xPosition;
+                state.RawPosition = pos;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Floor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace BustaGames.Climber
+{
+    [ExecuteInEditMode]
+    public class Floor : MonoBehaviour
+    {
+        public float floorHeight;
+
+        private Vector3 _nextAnchor;
+
+        private void Awake()
+        {
+            _nextAnchor = new Vector3(0, floorHeight, 0);
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Selection.activeObject == gameObject)
+            {
+                _nextAnchor = new Vector3(0, floorHeight, 0);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            var pos = transform.position;
+            Gizmos.DrawWireSphere(pos, 0.5f);
+            Gizmos.DrawWireSphere(pos + _nextAnchor, 0.5f);
+        }
+
+#endif
+    }
+}
